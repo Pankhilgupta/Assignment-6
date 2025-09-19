7f9c1b8c8c319c3ebac7b63c633d4816a777fc0b
Pankhilgupta <pankhilgupta2808@gmail.com>
2025-09-19 20:39:24 +0530

    Create Que2.cpp

---
+++ Que2.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* next;
+};
+Node* head = NULL;
+void insert(int x, int mode, int val) {
+    Node* temp = new Node;
+    Node* last = NULL;
+    Node* pre = NULL;
+    
+    temp->data = x;
+    temp->next = NULL;
+
+    if (head == NULL) {
+        head = temp;
+        head->next = temp;
+        return;
+    }
+
+    if (mode == 0) { // insert in beginning
+        Node* last = head;
+        while (last->next != head) {
+            last = last->next;
+        }
+        temp->next = head;
+        head = temp;
+        last->next = temp;
+    }
+    else if (mode == 2) { // insert at end
+        Node* last = head;
+        while (last->next != head) {
+            last = last->next;
+        }
+        last->next = temp;
+        temp->next = head;
+    }
+    else {
+        Node* temp1 = head;
+        while (temp1->next != head && temp1->data != val) {
+            pre=temp1;
+            temp1 = temp1->next;
+        }
+        if (mode == -1) { // insert before a data value
+            if(head->data == val){   // special case: value is at head
+            Node* last = head;
+            while(last->next != head){
+                last = last->next;
+            }
+            temp->next = head;
+            head = temp;
+            last->next = head;
+            }
+        else {
+            pre->next = temp;
+            temp->next = temp1;
+        }
+        }
+        else if (mode == 1) { // insert after a data value
+            temp->next = temp1->next;
+            temp1->next = temp;
+        }
+    }
+}
+
+void display() {
+    if (head == NULL) {
+        cout << "Empty list";
+        return;
+    }
+    Node* temp = head;
+    while (temp->next != head) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << temp->data<<" ";
+    temp=temp->next;
+    cout<<temp->data<<endl;
+}
+
+int main(){
+    insert(10,2,0);
+    insert(20,2,0);
+    insert(30,2,0);
+    insert(40,2,0);
+    display();
+    return 0;
+}
